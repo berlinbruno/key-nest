49cea736c45b10471fb45a7851959e41d7e24d3a
Berlin Bruno <berlinbruno112@gmail.com>
2024-08-16 03:07:52 +0530

    1.1

--- src/main/java/dev/berlinbruno/key_nest/config/SecurityConfig.java
+++ src/main/java/dev/berlinbruno/key_nest/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
-                                .requestMatchers("/login", "css/**", "js/**", "images/**", "/manifest.json").permitAll()
+                                .requestMatchers("/login", "/actuator/**", "css/**", "js/**", "images/**", "/manifest.json").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2Login ->
